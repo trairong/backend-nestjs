bfb4a340562b39c56fbb629732ffd283407e8e2a
Trairong <trairong.j@allrightbox.com>
2021-10-13 22:41:22 +0700

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SQLconfig } from './database/database.providers.config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(SQLconfig), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
